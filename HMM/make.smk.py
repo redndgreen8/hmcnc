ef8590fcb97c00b85ec0597abea980ce8533a4ec
redndgreen8 <37352469+redndgreen8@users.noreply.github.com>
2020-12-17 10:32:45 -0800

    make file for two cpp programs

---
+++ HMM/make.smk.py
@@ -0,0 +1,49 @@
+import os
+import tempfile
+import subprocess
+import os.path
+import json
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Location of boost include folder")
+parser.add_argument("--boost",help="Location of boost install/include folder. \
+	Most likely {anaconda install}/envs/\{proj_env\}/include")
+
+# Snakemake and working directories
+SD = os.path.dirname(workflow.snakefile)
+
+
+
+
+
+rule all:
+	input:
+		samtobed="samToBed",
+		vit="viterbi",
+
+rule stb:
+	input:
+		sTb="SamToBed.cpp",
+	output:
+		samtobed="samToBed",
+	shell:"""
+
+		g++ -02 {input.sTb} -o {output.samtobed}
+"""
+
+
+rule vit:
+	input:
+		vitt="viterbi.noclip.cpp",
+	output:
+		samtobed="viterbi",
+	params:
+		proj_env=args.boost,
+
+	shell:"""
+g++ -W -I {proj_env}/include {input.vitt} -o {samtobed}
+"""
+
+
+	
